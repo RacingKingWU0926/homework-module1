98c4a87863ac5fd05ed5b922e8b151a15fe028fa
JasonLei <jlei@activision.com>
2023-09-25 16:17:24 -0700

    disable button onload

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,7 +1,8 @@
-function generateCode() {
-    var code = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+var isDisabled;
 
+function generateCode() {
+    let code = '';
     for (i = 1; i < 9; i++) {
         var num = Math.random() * chars.length;
         code += chars.charAt(num);
@@ -11,8 +12,16 @@ function generateCode() {
 
 document.getElementById("codes").innerHTML = generateCode();
 
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(isDisabled) {
+    document.getElementById("submit").disabled = isDisabled;
+    console.log("hey!")
+
+    if (isDisabled == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-disableButton();
