6632eb8c28e0902293c5c333e1ab49065a2cadf8
Jason <143571915+RacingKingWU0926@users.noreply.github.com>
2023-09-25 21:18:14 -0700

    Week4 day2 (#11)
    * disable button onload
    
    * add codebox input event
    
    * updated CHANGELOG
    
    ---------
    
    Co-authored-by: JasonLei

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,7 +1,8 @@
-function generateCode() {
-    var code = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+var isDisabled;
 
+function generateCode() {
+    let code = '';
     for (i = 1; i < 9; i++) {
         var num = Math.random() * chars.length;
         code += chars.charAt(num);
@@ -9,10 +10,33 @@ function generateCode() {
     return code;
 }
 
-document.getElementById("codes").innerHTML = generateCode();
+// generate code
+const generated = generateCode();
+document.getElementById("codes").innerHTML = generated;
 
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+// verify generated & entered code
+function disableButton(isDisabled) {
+    document.getElementById("submit").disabled = isDisabled;
+
+    if (isDisabled == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-disableButton();
+// handle codebox input event
+function evaluateInput() {
+    var entered = document.getElementById("code-entered").value;
+    if (entered.length == generated.length && entered === generated) {
+        disableButton(false);
+    } else {
+        disableButton(true);
+    }
+}
+
+// listen to codebox input event
+var codebox = document.getElementById("code-entered")
+codebox.addEventListener("input", evaluateInput);
