0275cb7d57493f7b5314e9a137c5b6d93eb1f811
Jason <143571915+RacingKingWU0926@users.noreply.github.com>
2023-10-02 21:27:59 -0400

    Week5 day3 hw (#13)
    * added box for selected activities
    
    * display locations for activities
    
    * updated CHANGELOG
    
    ---------
    
    Co-authored-by: JasonLei

--- js/activity.js
+++ js/activity.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 
     // Add class "selectable" to all <td> cells (excluding "Not Available")
     $('td:not(.unselectable)').each(function () {
-        $cellVal = $(this).text().trim();
-        if ($cellVal === 'Not Available') {
+        var cellVal = $(this).text().trim();
+        if (cellVal === 'Not Available') {
             $(this).addClass('unselectable');
         } else {
             $(this).addClass('selectable');
@@ -19,11 +19,32 @@ $(document).ready(function() {
 
     // Click event: if the cell is already selected, remove it. Otherwise, add it.
     $('td.selectable').click(function () {
+        var activity = $(this).text().trim();
+
+        // Get the column index of the clicked cell, in order to get the column name
+        var cliffIndex = $(this).index();
+        var cliffName = $('th').eq(cliffIndex).text();
+
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
+            $(`#result p:contains(${activity})`).remove();
+
+            // if nothing is selected, hide the #displaySelected box
+            if ($("#result").has("p").length == false) {
+                $("#displaySelected").css({
+                    "visibility": "hidden",
+                    "margin": "0 auto 0 auto",
+                });
+            }
+
         } else {
             $(this).addClass('selected');
+            $("#result").append(`<p>${activity} at ${cliffName}</p>`);
+            $("#displaySelected").css({
+                "visibility": "visible",
+                "margin": "2em auto 2em auto",
+            });
         }
     });
 
-})
+});
